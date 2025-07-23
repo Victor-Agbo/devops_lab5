df8410c5aacb2e61533721657eb6556dc59a2a07
soram123 <wanglensoram8@gmail.com>
2025-07-23 12:23:11 +0530

    all files added

---
+++ src/main/java/com/example/myfirstmaven/MyfirstmavenApplication.java
@@ -0,0 +1,16 @@
+package com.example.myfirstmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstmavenApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(MyfirstmavenApplication.class, args);
+		System.out.println("hello world !");
+		
+	}
+
+}
