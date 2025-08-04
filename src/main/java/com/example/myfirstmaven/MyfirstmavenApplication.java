2911ec01fea035d3af06814a144efa1602648828
Soram Wanglen <57743241+soram123@users.noreply.github.com>
2025-08-04 13:44:11 +0530

    Update MyfirstmavenApplication.java

--- src/main/java/com/example/myfirstmaven/MyfirstmavenApplication.java
+++ src/main/java/com/example/myfirstmaven/MyfirstmavenApplication.java
@@ -4,17 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@RestController
 public class MyfirstmavenApplication {
-
-	@GetMapping("/")
-	    public String hello() {
-	        return "Hello from Spring Boot in Docker!";
-	    }
 
 	public static void main(String[] args) {
 		
 		SpringApplication.run(MyfirstmavenApplication.class, args);
+		System.out.println("Hello from Spring Boot in Docker!" );
 		
 		
 	}
